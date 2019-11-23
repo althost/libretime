althost/libretime

commit 45dbf8475060e14a56dae4d9389ff8546a69bc09

Generate artwork images for audio using ID3.

airtime_mvc/application/controllers/ScheduleController.php
@@ -122,7 +122,7 @@ class ScheduleController extends Zend_Controller_Action
         $currentUser = $service_user->getCurrentUser();
 
         $userTimezone = new DateTimeZone(Application_Model_Preference::GetUserTimezone());
-        
+
         $start = new DateTime($this->_getParam('start', null), $userTimezone);
         $start->setTimezone(new DateTimeZone("UTC"));
         $end = new DateTime($this->_getParam('end', null), $userTimezone);
@@ -187,7 +187,7 @@ class ScheduleController extends Zend_Controller_Action
             $this->view->show_error = true;
             return false;
         }
-        
+
         $error = $service_calendar->moveShow($deltaDay, $deltaMin);
         if (isset($error)) {
             $this->view->error = $error;
@@ -209,7 +209,7 @@ class ScheduleController extends Zend_Controller_Action
         $log_vars["params"]["delta day"] = $deltaDay;
         $log_vars["params"]["delta minute"] = $deltaMin;
         Logging::info($log_vars);
-        
+
         $userInfo = Zend_Auth::getInstance()->getStorage()->read();
         $user = new Application_Model_User($userInfo->id);
 
@@ -239,7 +239,7 @@ class ScheduleController extends Zend_Controller_Action
         $log_vars["params"] = array();
         $log_vars["params"]["instance id"] = $instanceId;
         Logging::info($log_vars);
-        
+
         $service_show = new Application_Service_ShowService();
         $showId = $service_show->deleteShow($instanceId, true);
 
@@ -261,7 +261,7 @@ class ScheduleController extends Zend_Controller_Action
     public function clearShowAction()
     {
         $instanceId = $this->_getParam('id');
-        
+
         $log_vars = array();
         $log_vars["url"] = $_SERVER['HTTP_HOST'];
         $log_vars["action"] = "schedule/clear-show";
@@ -296,12 +296,14 @@ class ScheduleController extends Zend_Controller_Action
 
         /* Convert all UTC times to localtime before sending back to user. */
         $range["schedulerTime"] = Application_Common_DateHelper::UTCStringToUserTimezoneString($range["schedulerTime"]);
-        
+
         if (isset($range["previous"])) {
             $range["previous"]["starts"] = Application_Common_DateHelper::UTCStringToUserTimezoneString($range["previous"]["starts"]);
             $range["previous"]["ends"] = Application_Common_DateHelper::UTCStringToUserTimezoneString($range["previous"]["ends"]);
         }
         if (isset($range["current"])) {
+            $get_artwork = FileDataHelper::getArtworkData($range["current"]["metadata"]["artwork"], 256);
+            $range["current"]["metadata"]["artwork_data"] = $get_artwork;
             $range["current"]["starts"] = Application_Common_DateHelper::UTCStringToUserTimezoneString($range["current"]["starts"]);
             $range["current"]["ends"] = Application_Common_DateHelper::UTCStringToUserTimezoneString($range["current"]["ends"]);
         }
@@ -309,14 +311,14 @@ class ScheduleController extends Zend_Controller_Action
             $range["next"]["starts"] = Application_Common_DateHelper::UTCStringToUserTimezoneString($range["next"]["starts"]);
             $range["next"]["ends"] = Application_Common_DateHelper::UTCStringToUserTimezoneString($range["next"]["ends"]);
         }
-  
+
         Application_Common_DateHelper::convertTimestamps(
-        	$range["currentShow"], 
+        	$range["currentShow"],
         	array("starts", "ends", "start_timestamp", "end_timestamp"),
         	"user"
         );
         Application_Common_DateHelper::convertTimestamps(
-        	$range["nextShow"], 
+        	$range["nextShow"],
         	array("starts", "ends", "start_timestamp", "end_timestamp"),
         	"user"
         );
@@ -324,7 +326,7 @@ class ScheduleController extends Zend_Controller_Action
         //TODO: Add timezone and timezoneOffset back into the ApiController's results.
         $range["timezone"] = Application_Common_DateHelper::getUserTimezoneAbbreviation();
         $range["timezoneOffset"] = Application_Common_DateHelper::getUserTimezoneOffset();
-        
+
         $source_status = array();
         $switch_status = array();
         $live_dj = Application_Model_Preference::GetSourceStatus("live_dj");
@@ -358,7 +360,7 @@ class ScheduleController extends Zend_Controller_Action
 
             return false;
         }
-        
+
         $originalShowId = $show->isRebroadcast();
         if (!is_null($originalShowId)) {
             try {
@@ -375,7 +377,7 @@ class ScheduleController extends Zend_Controller_Action
             $displayTimeZone = new DateTimeZone(Application_Model_Preference::GetTimezone());
             $originalDateTime = new DateTime($originalShowStart, new DateTimeZone("UTC"));
             $originalDateTime->setTimezone($displayTimeZone);
-            
+
             $this->view->additionalShowInfo =
                 sprintf(_("Rebroadcast of show %s from %s at %s"),
                     $originalShowName,
@@ -461,7 +463,7 @@ class ScheduleController extends Zend_Controller_Action
         $log_vars["params"] = array();
         $log_vars["params"]["form_data"] = $data;
         Logging::info($log_vars);
-        
+
         $service_showForm = new Application_Service_ShowFormService(
             $data["add_show_id"], $data["add_show_instance_id"]);
         $service_show = new Application_Service_ShowService(null, $data);
@@ -513,7 +515,7 @@ class ScheduleController extends Zend_Controller_Action
         if ($data['add_show_day_check'] == "") {
             $data['add_show_day_check'] = null;
         }
-        
+
         $log_vars = array();
         $log_vars["url"] = $_SERVER['HTTP_HOST'];
         $log_vars["action"] = "schedule/edit-show";
@@ -525,12 +527,12 @@ class ScheduleController extends Zend_Controller_Action
 
         list($data, $validateStartDate, $validateStartTime, $originalShowStartDateTime) =
             $service_showForm->preEditShowValidationCheck($data);
-        
+
         if ($service_showForm->validateShowForms($forms, $data, $validateStartDate,
                 $originalShowStartDateTime, true, $data["add_show_instance_id"])) {
             // Get the show ID from the show service to pass as a parameter to the RESTful ShowImageController
             $this->view->showId = $service_show->addUpdateShow($data);
-            
+
         	$this->view->addNewShow = true;
             $this->view->newForm = $this->view->render('schedule/add-show-form.phtml');
         } else {
@@ -541,7 +543,7 @@ class ScheduleController extends Zend_Controller_Action
                 $this->view->when->getElement('add_show_start_time')->setOptions(array('disabled' => true));
             }
             //$this->view->rr->getElement('add_show_record')->setOptions(array('disabled' => true));
-            
+
             $this->view->addNewShow = false;
             $this->view->action = "edit-show";
             $this->view->form = $this->view->render('schedule/add-show-form.phtml');
@@ -551,7 +553,7 @@ class ScheduleController extends Zend_Controller_Action
     public function addShowAction()
     {
         $service_showForm = new Application_Service_ShowFormService(null);
-        
+
         $js = $this->_getParam('data');
         $data = array();
 
@@ -565,20 +567,20 @@ class ScheduleController extends Zend_Controller_Action
         // TODO: move this to js
 		$data['add_show_hosts']     = $this->_getParam('hosts');
 		$data['add_show_day_check'] = $this->_getParam('days');
-		
+
         if ($data['add_show_day_check'] == "") {
             $data['add_show_day_check'] = null;
         }
-        
+
         $log_vars = array();
         $log_vars["url"] = $_SERVER['HTTP_HOST'];
         $log_vars["action"] = "schedule/add-show";
         $log_vars["params"] = array();
         $log_vars["params"]["form_data"] = $data;
         Logging::info($log_vars);
-        
+
         $forms = $this->createShowFormAction();
-        
+
         $this->view->addNewShow = true;
 
         if ($data['add_show_start_now'] == "now") {
@@ -597,18 +599,18 @@ class ScheduleController extends Zend_Controller_Action
         if ($service_showForm->validateShowForms($forms, $data)) {
         	// Get the show ID from the show service to pass as a parameter to the RESTful ShowImageController
         	$this->view->showId = $service_show->addUpdateShow($data);
-            
+
             //send new show forms to the user
             $this->createShowFormAction(true);
             $this->view->newForm = $this->view->render('schedule/add-show-form.phtml');
-            
+
             Logging::debug("Show creation succeeded");
         } else {
         	$this->view->form = $this->view->render('schedule/add-show-form.phtml');
             Logging::debug("Show creation failed");
         }
     }
-    
+
     public function createShowFormAction($populateDefaults=false)
     {
         $service_showForm = new Application_Service_ShowFormService();
@@ -638,7 +640,7 @@ class ScheduleController extends Zend_Controller_Action
     public function deleteShowAction()
     {
         $instanceId = $this->_getParam('id');
-        
+
         $log_vars = array();
         $log_vars["url"] = $_SERVER['HTTP_HOST'];
         $log_vars["action"] = "schedule/delete-show";
@@ -648,7 +650,7 @@ class ScheduleController extends Zend_Controller_Action
 
         $service_show = new Application_Service_ShowService();
         $showId = $service_show->deleteShow($instanceId);
-        
+
         if (!$showId) {
             $this->view->show_error = true;
         }
@@ -663,7 +665,7 @@ class ScheduleController extends Zend_Controller_Action
         $log_vars["params"] = array();
         $log_vars["params"]["instance id"] = $this->_getParam('id');
         Logging::info($log_vars);
-        
+
         $user = Application_Model_User::getCurrentUser();
 
         if ($user->isUserType(array(UTYPE_SUPERADMIN, UTYPE_ADMIN, UTYPE_PROGRAM_MANAGER))) {
@@ -730,7 +732,7 @@ class ScheduleController extends Zend_Controller_Action
     	$start = $this->_getParam('startTime');
     	$end = $this->_getParam('endTime');
     	$timezone = $this->_getParam('timezone');
-    	
+
         $service_showForm = new Application_Service_ShowFormService();
         $result = $service_showForm->calculateDuration($start, $end, $timezone);
 
@@ -741,10 +743,10 @@ class ScheduleController extends Zend_Controller_Action
     public function updateFutureIsScheduledAction()
     {
         $schedId = $this->_getParam('schedId');
-        
+
         $scheduleService = new Application_Service_SchedulerService();
         $redrawLibTable = $scheduleService->updateFutureIsScheduled($schedId, false);
-        
+
         $this->_helper->json->sendJson(array("redrawLibTable" => $redrawLibTable));
     }
 
@@ -762,5 +764,5 @@ class ScheduleController extends Zend_Controller_Action
 
         $this->_helper->json->sendJson($localTime);
     }
-    
+
 }

LICENSE_3RD_PARTY
@@ -22,7 +22,7 @@ Common Non-linked Code
 Airtime Web
 -----------
 Linked code:
- * Zend Framework 1.10.3 
+ * Zend Framework 1.10.3
    - What is it: Framework for PHP web apps
    - Web site: http://framework.zend.com/
    - License: New BSD license
@@ -32,7 +32,7 @@ Linked code:
    - What is it: PHP library
    - Notes: We only use the PEAR base class PEAR_Error, in the "PEAR" PEAR library.
    - License: New BSD License
-   - Compatible with GPLv3? Yes. 
+   - Compatible with GPLv3? Yes.
 
  * Propel ORM
    - What is it: Maps DB data into PHP objects
@@ -40,7 +40,7 @@ Linked code:
    - License: MIT/Expat License
    - Compatible with the GPL: Yes.  See http://www.gnu.org/licenses/license-list.html
 
- * Phing 
+ * Phing
    - What is it: PHP project build system
    - Web site: http://phing.info/trac/
    - Note: Only used for development, not needed to run Airtime.
@@ -48,22 +48,27 @@ Linked code:
 
  * PHP-AMQPLIB
    - What is it: PHP library to interact with RabbitMQ
-   - Web site: https://github.com/tnc/php-amqplib 
+   - Web site: https://github.com/tnc/php-amqplib
    - License: LGPLv2.1
    - Compatible with GPLv3? Yes
 
  * Soundcloud PHP API wrapper
    - What is it: PHP library to upload to SoundCloud
    - Web site: https://github.com/mptre/php-soundcloud/blob/master/Services/Soundcloud.php
-   - License: MIT   
+   - License: MIT
    - Compatible with the GPL: Yes.  See http://www.gnu.org/licenses/license-list.html
 
+ * getID3()
+   - What is it: PHP script that extracts useful information from MP3s & other multimedia file formats:
+   - Web site: https://github.com/JamesHeinrich/getID3
+   - License: GPLv3
+
 Non-linked code:
- * Apache Web Server 2.2 
+ * Apache Web Server 2.2
    - Web site: http://httpd.apache.org/
    - License: Apache 2.0. See http://httpd.apache.org/docs/2.2/license.html
 
- * PostgreSQL 9.1 
+ * PostgreSQL 9.1
    - Web site: http://www.postgresql.org/
    - License: The PostgreSQL License. See http://www.postgresql.org/about/licence
 
@@ -71,22 +76,22 @@ Non-linked code:
    - Web site: http://www.php.net/
    - License: The PHP License. See http://www.php.net/license/3_01.txt
 
- * jQuery 
+ * jQuery
    - Web site: http://jquery.com/
    - License: MIT and GPL.  See http://jquery.org/license
 
    - jQuery components used:
-     * Full Calendar 
+     * Full Calendar
        - Web site: http://arshaw.com/fullcalendar/
        - License: Dual licensed under MIT and GPLv2
 
-     * Colorpicker 
+     * Colorpicker
        - Web site: http://www.eyecon.ro/colorpicker/
        - License: Dual licensed under the MIT and GPL licenses.
 
      * Context Menu
        - Web site: http://jursza.net/dev/jjmenu/
-       - License: MIT [http://www.opensource.org/licenses/mit-license.php] 
+       - License: MIT [http://www.opensource.org/licenses/mit-license.php]
 
      * PLUpload
        - Web site: http://www.plupload.com
@@ -98,8 +103,8 @@ Non-linked code:
 
      * qtip
        - Web site: http://craigsworks.com/projects/qtip/
-       - License: MIT 
-       
+       - License: MIT
+
      * TimePicker
        - Web site: http://fgelinas.com/code/timepicker/
        - License: Dual licensed under the MIT or GPL Version 2 licenses.
@@ -120,7 +125,7 @@ Non-linked code:
 Media-Monitor
 -------------
 Linked code:
- * Mutagen 
+ * Mutagen
    - What is it: Parser of audio file metadata
    - Web site: http://code.google.com/p/mutagen/
    - License: GPLv2-only
@@ -129,7 +134,7 @@ Linked code:
    - What is it: Python interface to RabbitMQ
    - Web site: http://pypi.python.org/pypi/kombu/
    - License: New BSD
-   - Compatible with GPLv3? Yes. 
+   - Compatible with GPLv3? Yes.
 
  * pyinotify
    - Python interface to inotify
@@ -155,7 +160,7 @@ Linked code:
    - What is it: Python interface to RabbitMQ
    - Web site: http://pypi.python.org/pypi/kombu/
    - License: New BSD
-   - Compatible with GPLv3? Yes. 
+   - Compatible with GPLv3? Yes.
 
 Non-linked code:
  * Python 2.7
@@ -174,7 +179,7 @@ Linked code:
  * Kombu
    - Web site: http://pypi.python.org/pypi/kombu/
    - License: New BSD
-   - Compatible with GPLv3? Yes. 
+   - Compatible with GPLv3? Yes.
 
 Non-linked code:
  * Python 2.7

airtime_mvc/application/common/FileDataHelper.php
@@ -68,4 +68,275 @@ class FileDataHelper {
         }
     }
 
+    /**
+     * Gets data URI from artwork file
+     *
+     * @param string $file
+     * @param int $size
+     * @param string $filepath
+     *
+     * @return string Data URI for artwork
+     */
+    public static function getArtworkData($file, $size, $filepath = false)
+    {
+        $baseUrl = Application_Common_HTTPHelper::getStationUrl();
+        $default = $baseUrl . "css/images/no-cover.jpg";
+
+        if ($filepath != false) {
+            $path = $filepath . $file . "-" . $size;
+            if (!file_exists($path)) {
+                $get_file_content = $default;
+            } else {
+               $get_file_content = file_get_contents($path);
+            }
+        } else {
+            $storDir = Application_Model_MusicDir::getStorDir();
+            $path = $storDir->getDirectory() . $file . "-" . $size;
+            if (!file_exists($path)) {
+                $get_file_content = $default;
+            } else {
+               $get_file_content = file_get_contents($path);
+            }
+        }
+        return $get_file_content;
+    }
+
+    /**
+     * Add artwork file
+     *
+     * @param string $analyzeFile
+     * @param string $filename
+     * @param string $importDir
+     * @param string $DbPath
+     *
+     * @return string Path to artwork
+     */
+    public static function saveArtworkData($analyzeFile, $filename, $importDir = null, $DbPath = null)
+    {
+        $getID3 = new \getID3();
+        $getFileInfo = $getID3->analyze($analyzeFile);
+
+        if(isset($getFileInfo['comments']['picture'][0])) {
+
+              $get_img = "";
+              $timestamp = time();
+              $mime = $getFileInfo['comments']['picture'][0]['image_mime'];
+              $Image = 'data:'.$mime.';charset=utf-8;base64,'.base64_encode($getFileInfo['comments']['picture'][0]['data']);
+              $base64 = @$Image;
+
+              if (!file_exists($importDir . "/" . "artwork/")) {
+                  if (!mkdir($importDir . "/" . "artwork/", 0777, true)) {
+                      Logging::error("Failed to create artwork directory.");
+                      throw new Exception("Failed to create artwork directory.");
+                  }
+              }
+
+              $path_parts = pathinfo($filename);
+              $file = $importDir . "artwork/" . $path_parts['filename'];
+
+              //Save Data URI
+              if (file_put_contents($file, $base64)) {
+                  $get_img = $DbPath . "artwork/". $path_parts['filename'];
+              } else {
+                  Logging::error("Could not save Data URI");
+              }
+
+              if ($mime == "image/png") {
+                  $ext = 'png';
+              } elseif ($mime == "image/gif") {
+                  $ext = 'gif';
+              } elseif ($mime == "image/bmp") {
+                 $ext = 'bmp';
+              } else {
+                 $ext = 'jpg';
+              }
+
+              if (file_exists($file)) {
+                  self::resizeImage($file, $file . '-32.jpg', $ext, 32, 100);
+                  self::resizeImage($file, $file . '-64.jpg', $ext, 64, 100);
+                  self::resizeImage($file, $file . '-128.jpg', $ext, 128, 100);
+                  self::resizeImage($file, $file . '-256.jpg', $ext, 256, 100);
+                  self::resizeImage($file, $file . '-512.jpg', $ext, 512, 100);
+                  self::imgToDataURI($file . '-32.jpg', $file . '-32');
+                  self::imgToDataURI($file . '-64.jpg', $file . '-64');
+                  self::imgToDataURI($file . '-128.jpg', $file . '-128');
+                  self::imgToDataURI($file . '-256.jpg', $file . '-256');
+              } else {
+                  Logging::error("The file $file does not exist");
+              }
+        } else {
+              $get_img = '';
+        }
+        return $get_img;
+    }
+
+    /**
+     * Reset artwork
+     *
+     * @param string $trackid
+     *
+     * @return string $get_img Path to artwork
+     */
+    public static function resetArtwork($trackid)
+    {
+        $file = Application_Model_StoredFile::RecallById($trackid);
+        $md = $file->getMetadata();
+
+        $storDir = Application_Model_MusicDir::getStorDir();
+        $fp = $storDir->getDirectory();
+
+        $dbAudioPath = $md["MDATA_KEY_FILEPATH"];
+        $fullpath = $fp . $dbAudioPath;
+
+        $getID3 = new \getID3();
+        $getFileInfo = $getID3->analyze($fullpath);
+
+        if(isset($getFileInfo['comments']['picture'][0])) {
+
+              $get_img = "";
+              $mime = $getFileInfo['comments']['picture'][0]['image_mime'];
+              $Image = 'data:'.$getFileInfo['comments']['picture'][0]['image_mime'].';charset=utf-8;base64,'.base64_encode($getFileInfo['comments']['picture'][0]['data']);
+              $base64 = @$Image;
+
+              $audioPath = dirname($fullpath);
+              $dbPath = dirname($dbAudioPath);
+              $path_parts = pathinfo($fullpath);
+              $file = $path_parts['filename'];
+
+              //Save Data URI
+              if (file_put_contents($audioPath . "/" . $file, $base64)) {
+                  $get_img = $dbPath . "/" . $file;
+              } else {
+                  Logging::error("Could not save Data URI");
+              }
+
+              $rfile = $audioPath . "/" . $file;
+
+              if ($mime == "image/png") {
+                  $ext = 'png';
+              } elseif ($mime == "image/gif") {
+                  $ext = 'gif';
+              } elseif ($mime == "image/bmp") {
+                 $ext = 'bmp';
+              } else {
+                 $ext = 'jpg';
+              }
+
+              if (file_exists($rfile)) {
+                  self::resizeImage($rfile, $rfile . '-32.jpg', $ext, 32, 100);
+                  self::resizeImage($rfile, $rfile . '-64.jpg', $ext, 64, 100);
+                  self::resizeImage($rfile, $rfile . '-128.jpg', $ext, 128, 100);
+                  self::resizeImage($rfile, $rfile . '-256.jpg', $ext, 256, 100);
+                  self::resizeImage($rfile, $rfile . '-512.jpg', $ext, 512, 100);
+                  self::imgToDataURI($rfile . '-32.jpg', $rfile . '-32');
+                  self::imgToDataURI($rfile . '-64.jpg', $rfile . '-64');
+                  self::imgToDataURI($rfile . '-128.jpg', $rfile . '-128');
+                  self::imgToDataURI($rfile . '-256.jpg', $rfile . '-256');
+              } else {
+                  Logging::error("The file $rfile does not exist");
+              }
+        } else {
+              $get_img = "";
+        }
+        return $get_img;
+    }
+
+    /**
+     * Render image
+     * Used in API to render JPEG
+     *
+     * @param string $file
+     */
+    public static function renderImage($file)
+    {
+          $im = @imagecreatefromjpeg($file);
+          header('Content-Type: image/jpeg');
+          $img = $im;
+          imagejpeg($img);
+          imagedestroy($img);
+    }
+
+    /**
+     * Render Data URI
+     * Used in API to render Data URI
+     *
+     * @param string $dataFile
+     */
+    public static function renderDataURI($dataFile)
+    {
+          if($filecontent = file_get_contents($dataFile) !== false){
+               $image = @file_get_contents($dataFile);
+               $image = base64_encode($image);
+               if (!$image || $image === '') {
+                   return;
+               }
+               $blob = base64_decode($image);
+               $f = finfo_open();
+               $mime_type = finfo_buffer($f, $blob, FILEINFO_MIME_TYPE);
+               finfo_close($f);
+               header("Content-Type: " . $mime_type);
+               echo $blob;
+          } else {
+              return;
+          }
+    }
+
+    /**
+     * Resize Image
+     *
+     * @param string $orig_filename
+     * @param string $converted_filename
+     * @param string $ext
+     * @param string $size      Default: 500
+     * @param string $quality   Default: 75
+     *
+     */
+    public static function resizeImage($orig_filename, $converted_filename, $ext, $size=500, $quality=75)
+    {
+          $get_cont = file_get_contents($orig_filename);
+          if ($ext == "png") {
+              $im = @imagecreatefrompng($get_cont);
+          } elseif ($ext == "gif") {
+              $im = @imagecreatefromgif($get_cont);
+          } else {
+              $im = @imagecreatefromjpeg($get_cont);
+          }
+
+          if ($size){
+            $im = imagescale($im , $size);
+          }
+
+          if(!$im) {
+              $im  = imagecreatetruecolor(150, 30);
+              $bgc = imagecolorallocate($im, 255, 255, 255);
+              $tc  = imagecolorallocate($im, 0, 0, 0);
+              imagefilledrectangle($im, 0, 0, 150, 30, $bgc);
+              imagestring($im, 1, 5, 5, 'Error loading ' . $imgname, $tc);
+          }
+
+          $img = $im;
+          imagejpeg($img, $converted_filename, $quality);
+          imagedestroy($img);
+    }
+
+    /**
+     * Convert image to Data URI
+     *
+     * @param string $orig_filename
+     * @param string $conv_filename
+     */
+    public static function imgToDataURI($orig_filename, $conv_filename)
+    {
+          $file = file_get_contents($orig_filename);
+          $Image = 'data:image/jpeg;charset=utf-8;base64,'.base64_encode($file);
+          $base64 = @$Image;
+
+          //Save Data URI
+          if (file_put_contents($conv_filename, $base64)) {
+
+          } else {
+              Logging::error("Could not save Data URI");
+          }
+    }
+
 }

airtime_mvc/application/configs/constants.php
@@ -81,6 +81,8 @@ define('MDATA_KEY_REPLAYGAIN'  , 'replay_gain');
 define('MDATA_KEY_OWNER_ID'    , 'owner_id');
 define('MDATA_KEY_CUE_IN'      , 'cuein');
 define('MDATA_KEY_CUE_OUT'     , 'cueout');
+define('MDATA_KEY_ARTWORK'     , 'artwork');
+define('MDATA_KEY_ARTWORK_DATA', 'artwork_data');
 
 define('UI_MDATA_VALUE_FORMAT_FILE'   , 'File');
 define('UI_MDATA_VALUE_FORMAT_STREAM' , 'live stream');

commit e92e3d14fae3f4954a9e97d4b602cf5178e1fc99

When live streaming to LibreTime on ports 8001 or 8002, the MVC interface would break (500) I've found this is because of ScheduleController L305, introduced recently in https://github.com/LibreTime/libretime/commit/45dbf8475060e14a56dae4d9389ff8546a69bc09 A simple if clause allows us to workaround the issue, which must be that live streams don't have pictures nor metadata... Fixes #919

airtime_mvc/application/controllers/ScheduleController.php
@@ -302,8 +302,10 @@ class ScheduleController extends Zend_Controller_Action
             $range["previous"]["ends"] = Application_Common_DateHelper::UTCStringToUserTimezoneString($range["previous"]["ends"]);
         }
         if (isset($range["current"])) {
-            $get_artwork = FileDataHelper::getArtworkData($range["current"]["metadata"]["artwork"], 256);
-            $range["current"]["metadata"]["artwork_data"] = $get_artwork;
+            if (isset($range["current"]["metadata"])) {
+                $get_artwork = FileDataHelper::getArtworkData($range["current"]["metadata"]["artwork"], 256);
+                $range["current"]["metadata"]["artwork_data"] = $get_artwork;
+            }
             $range["current"]["starts"] = Application_Common_DateHelper::UTCStringToUserTimezoneString($range["current"]["starts"]);
             $range["current"]["ends"] = Application_Common_DateHelper::UTCStringToUserTimezoneString($range["current"]["ends"]);
         }